2e9921237389a6a3ba9478a4c5996417337a9913
HK <hykwong@stanford.edu>
2025-12-17 16:52:24 -0800

    phpBB - Updated Request.

--- phpBB33/vendor/symfony/http-foundation/Request.php
+++ phpBB33/vendor/symfony/http-foundation/Request.php
@@ -2010,7 +2010,7 @@ class Request
         }
 
         $pathInfo = substr($requestUri, \strlen($baseUrl));
-        if (false === $pathInfo || '' === $pathInfo) {
+        if (false === $pathInfo || '' === $pathInfo || '/' !== $pathInfo[0]) {
             // If substr() returns false then PATH_INFO is set to an empty string
             return '/';
         }
